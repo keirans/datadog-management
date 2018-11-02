af6039ccf640598b0e14d9392e5318c7b5cd1fe4
Andrew Tolopko <atolopko@chanzuckerberg.com>
2018-11-01 20:19:48 -0400

    Docker entrypoint fixes

--- ci/docker_entrypoint.sh
+++ ci/docker_entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-set -e
+set -ex
+
+echo meta-${ENV}-${SERVICE}
 
 eval `ssh-agent`
-GITHUB_DEPLOY_KEY_FILE=/tmp/github_deploy_key
-echo "${GITHUB_DEPLOY_KEY}" > $GITHUB_DEPLOY_KEY_FILE && chmod 600 $GITHUB_DEPLOY_KEY_FILE
+export GITHUB_DEPLOY_KEY_FILE=/tmp/github_deploy_key
+set +x
+chamber exec meta-${ENV}-${SERVICE} -- sh -c 'echo "$GITHUB_DEPLOY_KEY"' > $GITHUB_DEPLOY_KEY_FILE
+set -x
+chmod 600 $GITHUB_DEPLOY_KEY_FILE
 ssh-add $GITHUB_DEPLOY_KEY_FILE
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 git clone --depth 1 git@github.com:chanzuckerberg/meta-datadog-config.git
 
 cd /opt && \
-    chamber exec meta-${ENV}-${SERVICE_NAME} -- \
+    chamber exec meta-${ENV}-${SERVICE} -- \
     bundle exec ./datadog-backup-all.rb --backup-dir /meta-datadog-config --name-filter-regex '^meta-.*'
 
 cd /meta-datadog-config
